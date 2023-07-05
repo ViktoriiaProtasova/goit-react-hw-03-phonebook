75660e4d820d7717efb782ea361e3e5656a6b8e3
Viktoriia Protasova <viktorka.protasova@gmail.com>
2023-07-05 23:21:57 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,7 +20,7 @@ class App extends Component {
     }
   }
 
-  componentDidUpdate(prevState) {
+  componentDidUpdate(_, prevState) {
     if (prevState.contacts !== this.state.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
